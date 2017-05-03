03e225a8cba8f3253c188bb8d72cdc1e5e2b6fe3
Prof. Hodari Touré <hodari@hiddengeniusproject.org>
2017-05-03 13:17:14 -0700

    fixed sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
          has_many :chat_rooms, dependent: :destroy
          has_many :messages, dependent: :destroy
   def name
-    email.split('@')[0]
+    #email.split('@')[0]
   end
 end
---
+++ db/migrate/20170503200855_add_to_user.rb
@@ -0,0 +1,6 @@
+class AddToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users,  :first_name,  :string,  default: "", null: false
+    add_column :users,  :last_name, :string, default: "", null: false
+  end
+end
